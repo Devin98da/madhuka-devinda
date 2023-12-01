1699e5eae0f5ced5880cb3b575907c6b97d8eefe
Madhuka Devinda <72905366+Devin98da@users.noreply.github.com>
2023-12-01 12:31:36 -0800

    dfdf

--- src/Components/Header/CTA.jsx
+++ src/Components/Header/CTA.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 // import CV from '/cv.pdf';
 
 const CTA = () => {
+
+    const handleDownload = () => {
+      const pdfUrl = "https://devin98da.github.io" + '/madhuka-devinda.pdf';
+      window.open(pdfUrl, '_blank');
+    };
+
   return (
     <div className='cta'>
-        <a href="/madhuka-devinda.pdf" download='/madhuka-devinda.pdf' className='btn' target='blank' rel='noopener noreferrer'>Download CV</a>
+      <button onClick={handleDownload}>CLikc</button>
+        {/* <a href="/madhuka-devinda.pdf" download='/madhuka-devinda.pdf' className='btn' target='blank' rel='noopener noreferrer'>Download CV</a> */}
         <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
